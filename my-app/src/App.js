73b7ab689a6228592ea1ba8aa22b16c53b309191
apple <apple@Home.local>
2022-07-15 21:53:30 +0900

    change name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> NaruCCCCCCCha        </p>
+          Edit <code>src/App.js</code> こんにちは　</p>
         <a
           className="App-link"
           href="https://reactjs.org"
