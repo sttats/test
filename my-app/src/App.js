0554f5d69b5711cc795d9a34b116e09f48f6f6f3
apple <apple@Home.local>
2022-07-16 16:13:16 +0900

    再修正

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <p>こんにちは　</p>
+        <p> こんにちは　</p>
         <a
           className="App-link"
           href="https://reactjs.org"
